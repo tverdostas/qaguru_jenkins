ccc3f23daa1528ce6c57549416df9f1ef4726c95
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-09 23:14:19 +0400

    attachments added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,11 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
@@ -14,8 +17,12 @@ public class TestBase {
     }
 
     @AfterEach
-    public void closeWebDriver() {
-        Selenide.closeWebDriver();
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 
 }
