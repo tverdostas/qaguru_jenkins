bd5fb78e8ed2bbdb62dc8b5e6f0f0cab7623ba85
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-08-10 00:20:48 +0400

    browserConfigurations edited

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
